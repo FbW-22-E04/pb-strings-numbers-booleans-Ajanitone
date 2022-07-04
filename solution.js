b4ffa6cc51c0632c03e99192e23350d2469403f1
Ajanitone <ajanitone@freenet.de>
2022-07-04 11:28:10 +0200

    initial commit

--- solution.js
+++ solution.js
@@ -1,1 +1,33 @@
+const str = "strings";
 
+console.log(str);
+
+console.log("second element is", str[1]);
+console.log("the 6th element is", str[5]);
+
+const firstname = "Basem";
+const age = 37;
+const year = 1983;
+const when = age + year;
+const isMarried = true;
+
+console.log("", firstname + " is going to be", age + " in ", when);
+
+console.log("Is she married", isMarried);
+
+console.log("------------------------------------");
+
+const programmingLanguage = "JavaScript";
+const isFun = true;
+
+console.log("", programmingLanguage + " is fun:", isFun);
+
+console.log("------------------------------------");
+
+console.log("", programmingLanguage[0] + " and ", programmingLanguage[4]);
+
+console.log("------------------------------------");
+
+const lenghtOfJavaScript = programmingLanguage.length;
+
+console.log("The lenght og the string JavaScript is", lenghtOfJavaScript);
